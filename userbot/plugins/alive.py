f3318cadb406eaaa5c8a4fa676f195c3536fcc5b
kumar451 <66391155+kumar451@users.noreply.github.com>
2020-07-09 23:16:32 +0530

    Update alive.py
    New file add

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,7 +9,7 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_VID = "https://telegra.ph/file/447135c84c0dc62cb2f4a.mp4"
+ALIVE_IMG= "https://telegra.ph/file/fba20e6f52745cd6cd078.jpg"
 ALIVE_caption = "`FRIDAY IS:` **ONLINE**\n\n"
 ALIVE_caption += "**SYSTEM STATUS**\n\n"
 ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
